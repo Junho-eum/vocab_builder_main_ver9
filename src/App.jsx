e23b4e1aab989c0de723b5d302e61aa082f0dfcc
Junho Eum <jxe282@gmail.com>
2023-08-24 11:11:36 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -234,7 +234,7 @@ function App() {
         {remainingQuestions.length === 1 && (
           <h2 style={{ textAlign: "center" }}>Last Question</h2>
         )}
-        <h3>Intermed-07/02</h3>
+        <h3>Intermed-08/01</h3>
         <p>Meaning: {vocabList[index].word}</p>
         <form onSubmit={(e) => e.preventDefault()}>
           <input
